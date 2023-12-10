37ff31943d11d17eb8d50456ea457e6d551a8f16
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2023-12-10 19:12:55 +0400

    add roommate service

---
+++ roommate/src/main/java/com/microuni/roommate/RoommateApplication.java
@@ -0,0 +1,13 @@
+package com.microuni.roommate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoommateApplication.class, args);
+	}
+
+}
